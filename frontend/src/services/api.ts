d28a63fcc50af445bc25f91f106693039aaded62
Brayan <mmmarxcan@gmail.com>
2026-07-06 15:08:35 -0600

    Actualizamos la api url

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://pedida-backend.onrender.com/api](https://pedida-backend.onrender.com/api)';
 
 interface ProposalData {
     answer: string;
