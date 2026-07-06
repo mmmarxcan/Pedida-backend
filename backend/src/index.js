8e8c98c5d6c1241a8db076f4719e404eeef3d1aa
Brayan <mmmarxcan@gmail.com>
2026-07-06 15:18:09 -0600

    fix de render

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,21 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const frontendDistPath = path.resolve(__dirname, '../..', 'frontend', 'dist');
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(frontendDistPath));
 
 app.get('/api/health', (req, res) => {
     res.json({
@@ -18,12 +25,16 @@ app.get('/api/health', (req, res) => {
 });
 
 app.post('/api/proposal', (req, res) => {
-    const {answer, attemptsNo , timestamp} = req.body;
+    const { answer, attemptsNo, timestamp } = req.body;
     console.log(`Propuesta registrada: ${answer}, Intentos en No: ${attemptsNo}, Hora: ${timestamp}`);
-  res.json({ success: true, message: '¡Guardado correctamente en el servidor!' });
+    res.json({ success: true, message: '¡Guardado correctamente en el servidor!' });
 });
 
-app.listen(PORT,()=> {
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(frontendDistPath, 'index.html'));
+});
+
+app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
 
